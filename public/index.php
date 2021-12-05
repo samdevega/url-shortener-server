4c9ceaed7efbdb06eb0dfe966b05fd451e958359
samdevega <s.a.devega.garcia@gmail.com>
2021-12-05 23:36:56 +0000

    🔧 allow cors

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 define('LARAVEL_START', microtime(true));
 
 /*
